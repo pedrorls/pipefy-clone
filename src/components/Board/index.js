f164cd94b052a604b86ab552e081823bf4becd50
Pedro Silva <pedro.rls93@gmail.com>
2019-08-12 23:27:41 -0300

    fix api and styled components

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import List from "../List";
 import { Container } from './styles';
+import { loadLists } from '../Services/api';
+
+const lists = loadLists();
 
 export default function Board() {
   return (
     <Container>
-      <List/>
-      <List/>
-      <List/>
-      <List/>
+      {lists.map(list => <List key={list.title} data={list}/>)}
     </Container>
   );
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { Container, Label } from './styles';
 
-export default function Card() {
+export default function Card({data}) {
   return (
     <Container>
       <header>
-        <Label color="#B11" />
+        {data.labels.map(label => <Label key={label} color={label}/>)}
       </header>
-      <p>Fazer a migração completa do servidor</p>
-      <img src="https://api.adorable.io/avatars/285/abott@adorable.png" alt="" />
+      <p>{data.content}</p>
+      {data.user ? <img src={data.user} alt="" /> : null}
     </Container>
   );
 }
