cd0eec78c7a7bd70ce50ec37f542167bd13215bf
Pedro Silva <pedro.rls93@gmail.com>
2019-08-12 23:28:34 -0300

    import and folder position

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import List from "../List";
 import { Container } from './styles';
-import { loadLists } from '../Services/api';
+import { loadLists } from '../../Services/api';
 
 const lists = loadLists();
 
